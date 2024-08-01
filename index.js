f4f9936e15ac0058c02fc0c84ea21f52000b19c8
Nninex <118069914+Nninex@users.noreply.github.com>
2024-08-01 18:45:04 +0400

    Add calculator in index.js

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ function save() {
   count = 0;
   countEl.innerText = count;
 }
+
+let num1 = 8;
+let num2 = 2;
+
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+let resultEl = document.getElementById("result-el");
+function addition() {
+  let sum = num1 + num2;
+  resultEl.textContent = "Result: " + sum;
+}
+function subtraction() {
+  let sub = num1 - num2;
+  resultEl.textContent = "Result: " + sub;
+}
+function division() {
+  let div = num1 / num2;
+  resultEl.textContent = "Result: " + div;
+}
+function multiplication() {
+  let mul = num1 * num2;
+  resultEl.textContent = "Result: " + mul;
+}
