d7a545d5e881f2e7aacd5d9ea6c91aadda557eab
Nninex <118069914+Nninex@users.noreply.github.com>
2024-08-01 13:25:36 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+}
+function decrement() {
+  count -= 1;
+  countEl.innerText = count;
+}
+
+function save() {
+  let countDash = count + ";";
+  saveEl.innerText += " " + countDash;
+  count = 0;
+  countEl.innerText = count;
+}
